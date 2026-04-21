eae7f6a64b53bd3420862e06b512d5e5f91bbb18
Lillija <asikeligalillija@gmail.com>
2026-04-21 13:53:11 +0300

    Add basic routing and customers listing

--- public/index.php
+++ public/index.php
@@ -2,4 +2,19 @@
 
 require __DIR__ . '/../db/connect.php';
 
-echo "<h1>Veikals</h1>";
+$requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+if ($requestUri === '/customers') {
+
+    $stmt = $pdo->query("SELECT * FROM customers");
+    $customers = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    echo "<h1>Customers</h1>";
+
+    foreach ($customers as $c) {
+        echo "<p>{$c['first_name']} {$c['last_name']}</p>";
+    }
+
+} else {
+    echo "<h1>Veikals</h1>";
+}
