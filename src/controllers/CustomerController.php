99493dca0769c769eaccd9f0f3ec29a7dc2aad65
Lillija <asikeligalillija@gmail.com>
2026-04-21 15:01:04 +0300

    Always load customer orders in customers page for consistent UI

--- src/controllers/CustomerController.php
+++ src/controllers/CustomerController.php
@@ -5,13 +5,9 @@ require_once __DIR__ . '/../../db/DB.php';
 
 class CustomerController
 {
-    public static function index($withOrders = null)
+    public static function index()
     {
-        if ($withOrders === 'full') {
-            $customers = Customer::withOrders();
-        } else {
-            $customers = Customer::all();
-        }
+        $customers = Customer::withOrders();
 
         require __DIR__ . '/../views/customers.php';
     }
